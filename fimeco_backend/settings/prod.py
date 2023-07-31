e6cb5335aa866194679fe3f1ef90e0e9908f87d8
christian <christian@sahanalytics.com>
2023-07-31 19:14:07 +0000

    dockerisation

--- fimeco_backend/settings/prod.py
+++ fimeco_backend/settings/prod.py
@@ -1,1 +1,16 @@
 from .settings import *
+
+
+
+ALLOWED_HOSTS = ['192.168.1.3','localhost','127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': env.DB_NAME,
+        'USER': env.DB_USER,
+        'PASSWORD': env.DB_PASSWORD,
+        'HOST': env.DB_PASSWORD,
+        'PORT': env.DB_PORT
+    }
+}
